d963d9639025ae98810b95d6cb3250a62d5d6891
Misbahul Arifin <misbahularifin14@gmail.com>
2021-02-20 15:08:19 +0800

    deploy

---
+++ precache-manifest.59ea8a711dcdcd4521c393f42bf63fd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9bef1693a499711b77",
+    "url": "css/app.1acb83f4.css"
+  },
+  {
+    "revision": "1d7577e186f6af102f3c891863ddb48b",
+    "url": "img/profil.1d7577e1.png"
+  },
+  {
+    "revision": "a871c3ff349c2196105d404bf8243281",
+    "url": "index.html"
+  },
+  {
+    "revision": "b532031d6a5812c5458a",
+    "url": "js/about.07de7715.js"
+  },
+  {
+    "revision": "cf9bef1693a499711b77",
+    "url": "js/app.47ed2a20.js"
+  },
+  {
+    "revision": "b9019afb8fac4c11ce62",
+    "url": "js/chunk-vendors.d4c77e30.js"
+  },
+  {
+    "revision": "2b3c5b8af91700d2b70b4a8b2f4c4e57",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
